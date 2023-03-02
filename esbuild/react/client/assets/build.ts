3bba4f1cd58e9b60c4993f6c2e9e3736ebdd7faa
rayfill <rayfill@gmail.com>
2023-03-02 10:41:50 +0900

    support symlink reference

--- esbuild/react/client/assets/build.ts
+++ esbuild/react/client/assets/build.ts
@@ -1,5 +1,9 @@
+import { build, context } from 'esbuild';
+import type {
+  BuildContext, BuildOptions, Platform, ServeOptions, WatchOptions,
+  Plugin, PluginBuild, Message, Location,
+} from 'esbuild';
 import { typecheckPlugin } from '@jgoz/esbuild-plugin-typecheck';
-import { build, BuildContext, BuildOptions, context, Platform, ServeOptions, WatchOptions } from 'esbuild';
 import { dtsPlugin } from 'esbuild-plugin-d.ts';
 import postCssPlugin from 'esbuild-style-plugin';
 import tailwind from 'tailwindcss';
@@ -26,6 +30,78 @@ export enum ModuleType {
   CommonJS = 'cjs',
 };
 
+enum MessageType {
+  Warning,
+  Error,
+}
+interface HandlerResult {
+  type: MessageType;
+  location: Location | null;
+  message: Message;
+}
+
+function HandlerSortPredicate(lhs: HandlerResult, rhs: HandlerResult): number {
+  const lhsLocation = lhs.location;
+  const rhsLocation = rhs.location;
+  if (lhsLocation === null) {
+    return -1;
+  } else if (rhsLocation === null) {
+    return 1;
+  }
+
+  if (lhsLocation.file < rhsLocation.file) {
+    return -1;
+  } else if (lhsLocation.file > rhsLocation.file) {
+    return 1;
+  }
+  if (lhsLocation.line < rhsLocation.line) {
+    return -1;
+  } else if (lhsLocation.line > rhsLocation.line) {
+    return 1;
+  }
+  if (lhsLocation.column < rhsLocation.column) {
+    return -1;
+  } else if (lhsLocation.column > rhsLocation.column) {
+    return 1;
+  }
+  return 0;
+}
+
+function createMessageHandler(message: Message, messageType: MessageType): HandlerResult {
+  const handlerResult: HandlerResult = {
+    type: messageType,
+    location: message.location,
+    message,
+  }
+  return handlerResult;
+}
+
+function printMessages(messages: Array<HandlerResult>): void {
+  messages.forEach((message) => {
+    const location = message.message.location;
+    const place = location === null ? 'unknown' : `${location.file}:${location.line}:${location.column}`;
+    if (message.type === MessageType.Warning) {
+      logger.warn(place, message.message.text);
+    } else {
+      logger.error(place, message.message.text);
+    }
+  });
+}
+
+const IterationPlugin: Plugin = {
+  name: 'iteration-plugin',
+  setup(build: PluginBuild): void | Promise<void> {
+    build.onEnd(callback => {
+      const errorHandlers = callback.errors.map(error => createMessageHandler(error, MessageType.Error));
+      const warnHandlers = callback.warnings.map(warn => createMessageHandler(warn, MessageType.Warning));
+      const messages = errorHandlers.concat(warnHandlers).sort(HandlerSortPredicate);
+
+      printMessages(messages);
+      logger.info('compile finished');
+    });
+  }
+};
+
 const logger = new Logger({ minLevel: 3 });
 
 function createBuildOptions(
@@ -48,6 +124,7 @@ function createBuildOptions(
       '.js': module === ModuleType.ESModule ? '.mjs' : '.cjs',
     },
     outbase: 'src',
+    preserveSymlinks: true,
     loader: {
       '.png': 'dataurl',
       '.jpg': 'dataurl',
@@ -55,11 +132,18 @@ function createBuildOptions(
     },
     treeShaking: optimized,
     plugins: [
+      IterationPlugin,
       dtsPlugin({
         outDir: 'dist',
         tsconfig: 'tsconfig.json',
       }),
-      typecheckPlugin(),
+      typecheckPlugin({
+        configFile: 'tsconfig.json',
+        build: {
+          preserveSymlinks: true,
+          jsx: 'react-jsx',
+        }
+      }),
       postCssPlugin({
         postcss: {
           plugins: [
@@ -138,18 +222,26 @@ async function main() {
           servedir: config.servedir ?? 'static',
           host: 'localhost',
           port: 8888,
+          onRequest: (args) => {
+            logger.info('on request', args.method, args.path, args.status, args.timeInMS);
+          },
         };
         const serveResult = await buildContext.serve(serveOptions);
         logger.info(`serve on http://${serveResult.host}:${serveResult.port}/`);
       }
     } else {
       const buildResult = await build(buildOptions);
+      const errorHandlers = buildResult.errors.map(error => createMessageHandler(error, MessageType.Error));
+      const warnHandlers = buildResult.warnings.map(warn => createMessageHandler(warn, MessageType.Warning));
+      const messages = errorHandlers.concat(warnHandlers).sort(HandlerSortPredicate);
+
+      printMessages(messages);
+      logger.info('compile finished');
+
     }
   } catch (e) {
     logger.error(e);
     await buildContext?.dispose();
-  } finally {
-    logger.info('build finshed');
   }
 }
 
